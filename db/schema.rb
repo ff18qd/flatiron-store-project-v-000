a22e38d98ae2233000f03a2bf289296f3f63da86
EC2 Default User <ec2-user@ip-172-31-35-66.us-east-2.compute.internal>
2018-01-21 23:47:04 +0000

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -36,9 +36,19 @@ ActiveRecord::Schema.define(version: 20180121210450) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string  "email",              default: "", null: false
-    t.string  "encrypted_password", default: "", null: false
-    t.integer "current_cart_id"
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
