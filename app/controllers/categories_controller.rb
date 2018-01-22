5e0c77b59e6d11bb0919c0505f4654fa5d66f686
EC2 Default User <ec2-user@ip-172-31-35-66.us-east-2.compute.internal>
2018-01-22 03:24:40 +0000

    Done.

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,3 +1,9 @@
 class CategoriesController < ApplicationController
+    
+    def show
+        # binding.pry
+        @category = Category.find(params[:id])
+        @items = @category.items
+    end 
 
 end
